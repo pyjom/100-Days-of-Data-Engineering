d5998f5df0dafd6b0a8a1da3da78900ea860f157
Jomari <jomariarubio@gmail.com>
2024-03-13 16:53:35 +0800

    Continue game development

---
+++ Day-6/game.py
@@ -0,0 +1,14 @@
+"""
+THE GAME: Rock, Paper and Scissor
+Mechanics: A total of ten rounds will takeplace where the scores will be accumulated. 
+The winner will be announced right after the 10th round
+"""
+
+from random import randint
+
+# total of ten rounds
+for i in range(10):
+    print(randint(1,3))
+ 
+ # continue tomorrow
+
