7e99ca220d70e7e5bb089e1e6955b985753b2697
Jomari <jomariarubio@gmail.com>
2024-03-24 18:48:43 +0800

    Oop

--- Day-10/main.py
+++ Day-10/main.py
@@ -1,7 +1,33 @@
-# Creating our class
+srp = []
 
-class Item:
-    pass
 
-ngi
-print('Black')
+class Student():
+    def __init__(self, name, course, year, section):
+        self.name = name
+        self.course = course
+        self.year = year
+        self.section = section
+
+while True:
+    n = input('Please Enter your Name: ')
+    c = input('Program: ')
+    y = input('Year: ')
+    s = input('Section: ')
+    # calling the class
+    stu_data = Student(n,c,y,s) 
+    # store the stu_data to the list
+    srp.append(stu_data)
+
+    ngi = input('\n Create another student? [Y/Any char]').lower()
+    if ngi != 'y':
+        break
+    
+
+print(srp)
+
+
+# srr= dict
+# srr = {'ken': ['ngi', 123, 'pangit'], 'jom': 'pangit'}
+# print(srr)
+
+# dir(dict)
